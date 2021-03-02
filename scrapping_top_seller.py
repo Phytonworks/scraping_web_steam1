ae738f1d564a8ce785710715ce885d113f17f926
Eka Bernard <bernard.eka@gmail.com>
2021-03-02 19:27:59 +0700

    Scrapping web steam dengan request dan bs4 top Seller

---
+++ scrapping_top_seller.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://store.steampowered.com/vr/#p=0&tab=TopSellers'
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36'
+
+}
+r = requests.get(url, headers=headers)
+soup = BeautifulSoup(r.text, 'html.parser')
+top_sellers = soup.findAll('a', attrs={'class':'tab_item'})
+
+for top_seller in top_sellers:
+    print('Judul :', top_seller.find('div', attrs={'class': 'tab_item_name'}).text)
+    print('Gambar :', top_seller.find('div', attrs={'class': 'tab_item_cap'}).find('img'))
+    print('Tags :', top_seller.find('div', attrs={'class': 'tab_item_top_tags'}).text)
